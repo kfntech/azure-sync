5cc6d73ccb4f4edf1fa743e615bfb19aea0f9bc7
darknesszy <michaelziyumay@hotmail.com>
2020-02-25 14:04:37 +0800

    fix: git tracker tracking dist

--- dist/services/storage-cleaner.d.ts
+++
@@ -1,4 +0,0 @@
-import { Observable, Subject } from 'rxjs';
-import { ColumnValue } from 'tedious';
-export declare const setQuery$: Subject<string>;
-export declare const sqlDatabase$: Observable<ColumnValue>;
--- dist/services/storage-helper.d.ts
+++
@@ -1,4 +0,0 @@
-import { Observable, Subject } from 'rxjs';
-export declare const setContainer$: Subject<string>;
-export declare const blobContainer$: Observable<import("@azure/storage-blob").ContainerClient>;
-export declare const blobList$: Observable<import("@azure/storage-blob").BlobItem>;
